4602c6ab64ae40ff8d0cdf616dbf3e77fa049ba4
edgar <edgar59156@gmail.com>
2022-11-27 11:04:35 -0600

    push not

--- lib/services/push_notifications_services.dart
+++ lib/services/push_notifications_services.dart
@@ -5,9 +5,25 @@ class PushNotificationService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
 
+  static Future _backgroundHandler(RemoteMessage message) async {
+    print('background handler ${message.messageId}');
+  }
+
+  static Future _onMessageHandler(RemoteMessage message) async {
+    print('background handler ${message.messageId}');
+  }
+
+  static Future _onMessageOpenApp(RemoteMessage message) async {
+    print('background handler ${message.messageId}');
+  }
+
   static Future initializeApp() async {
     await Firebase.initializeApp();
     token = await FirebaseMessaging.instance.getToken();
     print('token: $token');
+
+    FirebaseMessaging.onBackgroundMessage(_backgroundHandler);
+    FirebaseMessaging.onMessage.listen(_onMessageHandler);
+    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageOpenApp);
   }
 }
