754889ce364dcca8845131ffe9089a0c7e3787fe
Edgar Gonzalez <edgar59156@gmail.com>
2022-11-27 11:16:23 -0600

    cambiado el services json google

--- lib/services/push_notifications_services.dart
+++ lib/services/push_notifications_services.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-
+//fbo_dlfXQ96aEOw65geCL6:APA91bF2JuxI587hm0YNprXo-k0nqYUOuJMTfQgbvH6iYaV2voLgVGXEEJIuUSMKLaZSv1XlejG19cDL6glE3GMmtrQWfPl_raRM7pJ_JEkMeZH8FaAevnGhKKRSjluky09RGxNENwdF
 class PushNotificationService {
   static FirebaseMessaging messaging = FirebaseMessaging.instance;
   static String? token;
